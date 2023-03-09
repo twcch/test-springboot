d04fb00ec827b177ba0f7ad4afccbd247cc1ff40
twcch <twcch1218@gmail.com>
2023-03-09 22:21:17 +0800

    update

--- src/main/java/com/twcch/testspringboot/controller/ParamController.java
+++ src/main/java/com/twcch/testspringboot/controller/ParamController.java
@@ -9,18 +9,21 @@ import java.util.Map;
 @RequestMapping("/param/v1/")
 public class ParamController {
 
-    @GetMapping("/getbook/{id}")
+    @GetMapping("/getbook/{id}/{name}")
     /*
      * @PathVariable 獲取 url 中 id 的註解
      */
-    public Object getBook(@PathVariable long id) {
+    public Object getBook(@PathVariable long id,
+                          @PathVariable String name) {
 
         System.out.println(" --- id: " + id);
+        System.out.println(" --- name: " + name);
 
         Map<String, Object> book = new HashMap<>();
         book.put("name", "互聯網世界觀");
         book.put("isbn", "9877234623432");
         book.put("author", "Alan");
+        book.put("user", name);
 
         return book;
     }
