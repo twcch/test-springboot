178493716889c202449838186ecf84e9c7c5a9c4
twcch <twcch1218@gmail.com>
2023-03-09 23:19:09 +0800

    update

--- src/main/java/com/twcch/testspringboot/controller/TestController.java
+++ src/main/java/com/twcch/testspringboot/controller/TestController.java
@@ -1,54 +1,26 @@
 package com.twcch.testspringboot.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /*
- * @RestController:
- * 主要用於更加快速的建構 RESTful Web 服務
- * 將 class 標示為外部控制器
- * 方法下的 return 如果是一個 String 就會 return(映射) 一個 String
- * 方法下的 return 如果是一個實例對象就會 return(映射) 一個 json 格式
+ * @Controller:
+ * 方法下的 return 如果是一個 String 就會 return(映射) 一個模板
  */
-@RestController
-/*
- * @RequestMapping 用於 class
- * url rule: http://localhost:8080/api/version/name
- */
-@RequestMapping("/test1/v1/")
+@Controller
+@RequestMapping("/testcontroller/v1/")
 public class TestController {
 
     /*
-     * @RequestMapping 用於方法
-     * @RequestMapping: 將方法標示為處理外部請求
+     * @Controller + @RequestMapping 方法下的 return 如果是一個 String 就會 return(映射) 一個模板
+     * @Controller + @ResponseBody = @RestController 映射的效果相同
      */
     @RequestMapping("/hello")
+    @ResponseBody
     public String hello() {
         return "Hello!";
     }
 
-    /*
-     * 聲明處理請求的類型:
-     * @RequestMapping(value = "url", method = RequestMethod.GET)
-     * @RequestMapping(value = "url", method = RequestMethod.POST)
-     * @RequestMapping(value = "url", method = RequestMethod.PUT)
-     * @RequestMapping(value = "url", method = RequestMethod.DELETE)
-     * 如果沒有限制 method 表示所有的請求都支持
-     */
-    @RequestMapping(value = "/gethello", method = RequestMethod.GET)
-    public String getHello() {
-        return "Hi!";
-    }
-
-    /*
-     * @RequestMapping 的簡寫形式
-     * @GetMapping("url")
-     * @PostMapping("url")
-     * @PutMapping("url")
-     * @DeleteMapping("url")
-     */
-    @GetMapping("/sgethello")
-    public String sGetHello() {
-        return "Hi!";
-    }
 
 }
---
+++ src/main/java/com/twcch/testspringboot/controller/TestRestController.java
@@ -0,0 +1,54 @@
+package com.twcch.testspringboot.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/*
+ * @RestController:
+ * 主要用於更加快速的建構 RESTful Web 服務
+ * 將 class 標示為外部控制器
+ * 方法下的 return 如果是一個 String 就會 return(映射) 一個 String
+ * 方法下的 return 如果是一個實例對象就會 return(映射) 一個 json 格式
+ */
+@RestController
+/*
+ * @RequestMapping 用於 class
+ * url rule: http://localhost:8080/api/version/name
+ */
+@RequestMapping("/testrestcontroller/v1/")
+public class TestRestController {
+
+    /*
+     * @RequestMapping 用於方法
+     * @RequestMapping: 將方法標示為處理外部請求
+     */
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+
+    /*
+     * 聲明處理請求的類型:
+     * @RequestMapping(value = "url", method = RequestMethod.GET)
+     * @RequestMapping(value = "url", method = RequestMethod.POST)
+     * @RequestMapping(value = "url", method = RequestMethod.PUT)
+     * @RequestMapping(value = "url", method = RequestMethod.DELETE)
+     * 如果沒有限制 method 表示所有的請求都支持
+     */
+    @RequestMapping(value = "/gethello", method = RequestMethod.GET)
+    public String getHello() {
+        return "Hi!";
+    }
+
+    /*
+     * @RequestMapping 的簡寫形式
+     * @GetMapping("url")
+     * @PostMapping("url")
+     * @PutMapping("url")
+     * @DeleteMapping("url")
+     */
+    @GetMapping("/sgethello")
+    public String sGetHello() {
+        return "Hi!";
+    }
+
+}
