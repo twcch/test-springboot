1e1dd1c74b24e7168335ec9e7e3a4ded98223c9a
twcch <twcch1218@gmail.com>
2023-03-09 22:45:40 +0800

    update

--- src/main/java/com/twcch/testspringboot/controller/ParamController.java
+++ src/main/java/com/twcch/testspringboot/controller/ParamController.java
@@ -9,6 +9,9 @@ import java.util.Map;
 @RequestMapping("/param/v1/")
 public class ParamController {
 
+    /*
+     * 獲取一條書單
+     */
     @GetMapping("/getbook/{id}/{name}")
     /*
      * @PathVariable 獲取 url 中 id 的註解
@@ -24,6 +27,29 @@ public class ParamController {
         book.put("isbn", "9877234623432");
         book.put("author", "Alan");
         book.put("user", name);
+
+        return book;
+    }
+
+    /*
+     * 新增一條書單
+     */
+    @PostMapping("/postbook")
+    /*
+     * @RequestParam 接收前端傳的參數
+     */
+    public Object postBook(@RequestParam String name,
+                           @RequestParam String author,
+                           @RequestParam String isbn) {
+
+        System.out.println(" --- name: " + name);
+        System.out.println(" --- author: " + author);
+        System.out.println(" --- isbn: " + isbn);
+
+        Map<String, Object> book = new HashMap<>();
+        book.put("name", name);
+        book.put("author", author);
+        book.put("isbn", isbn);
 
         return book;
     }
