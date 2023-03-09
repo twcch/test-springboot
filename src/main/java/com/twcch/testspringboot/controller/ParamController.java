a9844050e1f70c0e30abea58ca8fc45c0641c56f
twcch <twcch1218@gmail.com>
2023-03-09 21:56:34 +0800

    update

---
+++ src/main/java/com/twcch/testspringboot/controller/ParamController.java
@@ -0,0 +1,28 @@
+package com.twcch.testspringboot.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/param/v1/")
+public class ParamController {
+
+    @GetMapping("/getbook/{id}")
+    /*
+     * @PathVariable 獲取 url 中 id 的註解
+     */
+    public Object getBook(@PathVariable long id) {
+
+        System.out.println(" --- id: " + id);
+
+        Map<String, Object> book = new HashMap<>();
+        book.put("name", "互聯網世界觀");
+        book.put("isbn", "9877234623432");
+        book.put("author", "Alan");
+
+        return book;
+    }
+
+}
