6db84657f708e54cd8ca19ae31ada525f2709e48
twcch <twcch1218@gmail.com>
2023-03-09 20:54:38 +0800

    update

--- src/main/java/com/twcch/testspringboot/controller/DemoController.java
+++ src/main/java/com/twcch/testspringboot/controller/DemoController.java
@@ -3,10 +3,18 @@ package com.twcch.testspringboot.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * RestController: 將 class 標示為外部控制器
+ * RestController 下的 return 如果是一個 String 就會 return 一個 String
+ * RestController 下的 return 如果是一個實例對象就會 return 一個 json 格式
+ */
 @RestController
 public class DemoController {
 
-    @RequestMapping("/helloworld")
+    /*
+     * url rule: http://localhost:8080/api/version/name
+     */
+    @RequestMapping("/testspringboot/v1/helloworld")
     public String helloworld() {
         return "Hello World!";
     }
